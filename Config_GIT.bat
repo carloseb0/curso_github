3038a1d1b2de7bed658cf13923fb46e1aa3ca26d
carloseb0 <carloseduardo.b27@hotmail.com>
2022-08-09 19:26:11 -0300

    Commit de 09/08/2022-19h26m11s

---
+++ Config_GIT.bat
@@ -0,0 +1,6 @@
+git config --global user.name "carloseb0"
+git config --global user.email "carloseduardo.b27@hotmail.com"
+git init --initial-branch=main
+git remote add origin https://github.com/carloseb0/curso_github.git
+
+timeout 5
